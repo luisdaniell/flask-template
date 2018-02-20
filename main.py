467df572c12b4366f85c02a1eb36426da1f3af68
luis <luis@daniel>
2018-02-19 20:03:01 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+from flask import render_template
+from flask import request
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True , port = 8002)
